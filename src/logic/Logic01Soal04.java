5b3620ff910ce1352ecee17038f53edd810879a1
aldirif <97742642+AldiRif@users.noreply.github.com>
2022-12-14 09:01:19 +0700

    Initial commit

---
+++ src/logic/Logic01Soal04.java
@@ -0,0 +1,23 @@
+package logic;
+
+public class Logic01Soal04 extends BasicLogic{
+    public Logic01Soal04(int n) {
+        super(n);
+    }
+
+    public void isiArray(){
+        int angka = 1;
+        int angka1 = 1;
+        for (int i = 0; i < this.n; i++) {
+            this.array[0][i]= String.valueOf(angka);
+            int sum = angka + angka1;
+            angka = angka1;
+            angka1 = sum;
+        }
+    }
+
+    public void cetakArray(){
+        this.isiArray();
+        this.printSingle();
+    }
+}
